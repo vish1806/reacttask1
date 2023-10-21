3db1c974e8cf6a221d516c20a32c7255efc3e0aa
abc <abc@example.com>
2023-10-21 14:07:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WordCounter from './Components/countWord';
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter/> 
+    </div>
+  );
+}
+
+export default App;
